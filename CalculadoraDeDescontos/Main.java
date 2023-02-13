b65c824438574ead1c649ba9e54ce679e1fc75d6
EricVzV <erichramos2005@gmail.com>
2023-02-13 14:00:12 -0300

    fiz um comentário "exemplo"

--- CalculadoraDeDescontos/Main.java
+++ CalculadoraDeDescontos/Main.java
@@ -5,7 +5,8 @@ package CalculadoraDeDescontos;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //exemplo
         
+
     }
 }
